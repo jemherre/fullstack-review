7492571eea9fcdeb34588140a14f3d8b6ba71961
Jessica <jessica.herrera82094@gmail.com>
2019-01-26 02:46:04 -0800

    Complete displaying top 25 repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 const RepoList = (props) => (
   <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    {/* <h4> Repo List Component </h4>
+    There are {props.repos.length} repos. */}
+    <ul>
+      {RenderRepo(props.repos)} 
+    </ul>
   </div>
 )
+
+const SingleRepo = (doc) => {
+  return <li><a href='#' onClick={(e)=>{console.log(e)}}>{doc.repo.name}</a></li>;
+} 
+
+var RenderRepo = function(list){
+  var html = [];
+  list.map((doc)=>{
+    html.push(<SingleRepo repo={doc} key={doc.name}/>);
+  });
+
+  return html;
+};
 
 export default RepoList;
